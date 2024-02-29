cda107bd7cbf9326e1b274adf776547b89691bd4
NicolasTre <N.TREZEGUET@lahorde.school>
2024-02-29 16:06:43 +0100

    Creation of script for Scriptable Object and creation of scriptable object
    creation of the start of bonus

---
+++ Assets/Script/BombeData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BombeData", menuName = "Game/BombeData")]
+public class BombeData : ScriptableObject
+{
+    public int m_Radius;
+}
---
+++ Assets/Script/IAData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IAData", menuName = "Game/IAData")]
+public class IAData : ScriptableObject
+{
+   [SerializeField] private float m_Speed;
+}
