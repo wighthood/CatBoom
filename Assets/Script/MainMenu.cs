1586839926a3b215346d6559bbf0a83cb10f8e31
NicolasTre <N.TREZEGUET@lahorde.school>
2024-03-01 11:32:11 +0100

    add good menu , a new scene

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject m_Credits;
+    [SerializeField] private GameObject m_Menu;
+    [SerializeField] private GameObject m_Settings;
+
+    public void OnPlayBtn()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+    public void OnCreditsBtn()
+    {
+        m_Menu.SetActive(false);
+        m_Credits.SetActive(true);
+        Debug.Log("ggg");
+    }
+
+    public void OnSettingsBtn()
+    {
+        m_Menu.SetActive(false);
+        m_Credits.SetActive(true);
+    }
+
+    public void OnLeaveBtn()
+    {
+        Application.Quit();
+    }
+
+}
