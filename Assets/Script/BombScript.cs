72a11be94765f02c9f8f160b21e4756e4298d40b
wighthood <zefires@gmail.com>
2024-03-04 13:35:44 +0100

    still working on bombs

--- Assets/Script/BombScript.cs
+++ Assets/Script/BombScript.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class BombScript : MonoBehaviour
 {
+    [SerializeField] private GameObject m_Explosion;
+    [SerializeField] private int range;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,7 +13,16 @@ public class BombScript : MonoBehaviour
 
     IEnumerator bombTimer()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
+        Instantiate(m_Explosion, transform);
+        for (int i = 1; i <= range; i++)
+        {
+            Instantiate(m_Explosion, transform);
+            Instantiate(m_Explosion, transform);
+            Instantiate(m_Explosion, transform);
+            Instantiate(m_Explosion, transform);
+        }
+        yield return new WaitForSeconds(2);
         Object.Destroy(gameObject);
     }
 }
