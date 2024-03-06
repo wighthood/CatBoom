b732dec41d1adcc54e71ae487e02fe01ec6e0ccb
wighthood <zefires@gmail.com>
2024-03-06 17:18:30 +0100

    working bomb
    just need to detect collision with player

--- Assets/Script/BombScript.cs
+++ Assets/Script/BombScript.cs
@@ -5,6 +5,7 @@ public class BombScript : MonoBehaviour
 {
     [SerializeField] private GameObject m_Explosion;
     [SerializeField] private int range;
+    [SerializeField] private LayerMask m_layerMask;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +18,10 @@ public class BombScript : MonoBehaviour
         yield return new WaitForSeconds(4);
 
         Instantiate(m_Explosion, transform.position, Quaternion.identity);
-        StartCoroutine(CreateExplosion(Vector3.up/2));
-        StartCoroutine(CreateExplosion(Vector3.right/2));
-        StartCoroutine(CreateExplosion(Vector3.down/2));
-        //StartCoroutine(CreateExplosion(Vector3.left/2));
+        StartCoroutine(CreateExplosion(Vector3.up));
+        StartCoroutine(CreateExplosion(Vector3.right));
+        StartCoroutine(CreateExplosion(Vector3.down));
+        StartCoroutine(CreateExplosion(Vector3.left));
         yield return new WaitForSeconds(1);
         Destroy(gameObject);
     }
@@ -29,14 +30,17 @@ public class BombScript : MonoBehaviour
     {
         for (int i = 1; i <= range; i++)
         {
-            RaycastHit hit;
-            Physics.Raycast(transform.position + new Vector3(0, range, 0), direction, out hit,i);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction,i,m_layerMask);
             if (!hit.collider)
             {
-                Instantiate(m_Explosion, transform.position + (i * direction), m_Explosion.transform.rotation);
+                Instantiate(m_Explosion, transform.position + (0.5f*i* direction), m_Explosion.transform.rotation);
             }
-            else if (hit.collider.CompareTag("Indestructible") || hit.collider.CompareTag("Wall"))
+            else
             {
+                if(hit.collider.CompareTag("Wall"))
+                {
+                    Destroy(hit.collider.transform.gameObject);
+                }
                 break;
             }
             yield return new WaitForSeconds(.01f);
